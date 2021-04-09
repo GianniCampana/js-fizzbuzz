443edf787578f342f4c119d689be07e6b7eeafcb
GianniCampana <campanagianni97@gmail.com>
2021-04-09 15:38:54 +0200

    FizzBuzz

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var numeri_casuali = Math.floor(Math.random() * 100) + 1;
+document.getElementById("numeri").innerHTML = "Il numero estratto è " + numeri_casuali;
+
+if(numeri_casuali % 3 == 0){
+    document.getElementById("numeri").innerHTML = "Il numero estratto è " + "Fizz";
+}else if(numeri_casuali % 5 == 0){
+    document.getElementById("numeri").innerHTML = "Il numero estratto è " + "Buzz";
+}else if(numeri_casuali % 3 == 0 && numeri_casuali % 5 == 0 ){
+    document.getElementById("numeri").innerHTML = "Il numero estratto è " + "FizzBuzz";
+}
